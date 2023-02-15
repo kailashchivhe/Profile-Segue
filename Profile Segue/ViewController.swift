c81eb9700300550d41f63c54fc0f0a74d00e431a
Kailash Chivhe <kchivhe@gmail.com>
2023-02-15 13:21:52 -0500

    Initial Commit

---
+++ Profile Segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Profile Segue
+//
+//  Created by Kailash Chivhe on 15/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
